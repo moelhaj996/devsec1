aa18e3169ee7bbe09df0ab9fe720e33a063aa79a
moelhaj996 <mohsuliman.996@gmail.com>
2025-04-03 14:39:27 +0200

    update

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,4 +1,10 @@
 import pytest
+import sys
+import os
+
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 @pytest.fixture
